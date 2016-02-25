a3920500999993746cc55c04fd15dd3fcfc8ff88
fishTao <tao_0504@sina.com>
2016-02-25 15:09:09 +0800

    Initial Commit

---
+++ 顶部滑动菜单/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  顶部滑动菜单
+//
+//  Created by qingyun on 16/2/25.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
